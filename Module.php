d1670fb4663fcf0e2caae0ae61652373cbe60833
Rob Caiger <rob@clocal.co.uk>
2014-12-31 10:33:27 +0000

    Moved module.php

--- Module.php
+++ Module.php
@@ -1,35 +1,3 @@
 <?php
 
-namespace Dispatcher;
-
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
-/**
- * Module config
- */
-class Module
-{
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
+require_once __DIR__ . '/src/Dispatcher/Module.php';
